421168289470493e8f788ed761e7a5709d523cea
AOISORA072101 <skyblue092101@gmail.com>
2016-12-03 20:41:07 +0900

    first commit

---
+++ src/main/java/com/example/GsLineBotApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsLineBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsLineBotApplication.class, args);
+	}
+}
