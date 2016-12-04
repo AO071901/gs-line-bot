19eba442f4b53f112a0de718f5cf9d781915c488
AOISORA072101 <skyblue092101@gmail.com>
2016-12-04 13:48:25 +0900

    update

--- src/main/java/com/example/GsLineBotApplication.java
+++ src/main/java/com/example/GsLineBotApplication.java
@@ -3,10 +3,29 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 @SpringBootApplication
+@LineMessageHandler
 public class GsLineBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GsLineBotApplication.class, args);
+	}
+
+	@EventMapping
+	public TextMessage handlerTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		System.out.println("event: " + event);
+		return new TextMessage(event.getMessage().getText());
+	}
+
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("event: " + event);
 	}
 }
